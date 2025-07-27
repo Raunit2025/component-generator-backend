3d17ffc6eec5f3d7e8ca7308c10db80e6d9ad899
Raunit2025 <raunitraj2336@gmail.com>
2025-07-27 19:20:13 +0530

    fianl

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log('Connecting to MongoDB with URI:', process.env.MONGO_URL); 
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
