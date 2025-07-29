1a08ba704fb8ce0256cc4bfd8c4f758348335a49
Raunit Raj <103110498+Raunit2025@users.noreply.github.com>
2025-07-29 11:31:56 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,6 @@
 // middleware/authMiddleware.js
 const passport = require('passport');
 
-// This middleware uses the 'jwt' strategy and ensures no session is created
 const protect = passport.authenticate('jwt', { session: false });
 
 module.exports = { protect };
